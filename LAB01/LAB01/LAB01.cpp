b75e5990dd946219787178b09ee0da6730b134f4
Peter Kuzmichev <peter.kuzmicheff@yandex.ru>
2020-09-22 11:38:42 +0300

    First tasks are done

--- LAB01/LAB01/LAB01.cpp
+++ LAB01/LAB01/LAB01.cpp
@@ -1,8 +1,13 @@
 // LAB01.cpp: определяет точку входа для консольного приложения.
 //
+
+#define _USE_MATH_DEFINES
+
 #include <iostream>
 #include <string>
 #include "stdafx.h"
+#include <cmath>
+#include <math.h>
 
 using namespace std;
 
@@ -14,6 +19,62 @@ void second(){
 	char ch;
 	cin >> ch;
 	cout << 3 << ch << 13 << ch << 49;
+}
+
+void third(){
+	char ch;
+	cin >> ch;
+	int a, b, c;
+	cin >> a >> b >> c;
+	cout << a << ch << b << ch << c;
+}
+
+int func_x(int a){
+	return 12 * a * a + 7 * a - 12;
+}
+
+int func_y(int x){
+	return 3 * x * x * x + 4 * x * x - 11 * x + 1; 
+}
+
+double density(double m, double v){
+	return m / v;
+}
+
+double linear(double a, double b){
+	return (-1) * b / a;
+}
+
+double distance(double x1, double x2, double y1, double y2){
+	return sqrt(pow((x1 - x2), 2) + pow((y1 - y2), 2));
+}
+
+double geometry(double a, double b, double h){
+	return a + b + 2 * sqrt(pow(abs(b - a), 2) +  h*h);
+}
+
+double ring(double r_1, double r_2){
+	return abs(M_PI * r_1 * r_1 - M_PI * r_2 * r_2);
+}
+
+double cube_volume(double a){
+	return (a * a * a);
+}
+
+double side(double a){
+	return (a * a);
+}
+
+double square_perim(double a){
+	return (a * 4);
+}
+
+double rad_to_d(double r){
+	return r * 2;
+}
+
+void hello(string s){
+	cout << "Hello, " << s << "! My name is C++.";
 }
 
 void is_polindrom(){
@@ -31,9 +92,6 @@ int _tmain(int argc, _TCHAR* argv[])
 	char ch;
 	string s;
 	long a, b, c;
-	is_polindrom();
-	first();
-	second();
 	system("pause");
 	return 0;
 }
